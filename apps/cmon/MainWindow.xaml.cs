7796f61930f0d2529af15cfe9a2b359a5b7c8f71
pu1s <pu1s@outlook.com>
2018-08-26 13:04:46 +0400

    ++

--- apps/cmon/MainWindow.xaml.cs
+++ apps/cmon/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace cmon
 {
     /// <summary>
@@ -20,9 +21,16 @@ namespace cmon
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
