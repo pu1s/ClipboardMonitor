efdd8d7c13b2a033e963f2482589f1613b0b433e
Горин Александр <pu1s@outlook.com>
2018-09-13 23:38:56 +0400

    add x64 solution

--- apps/WPF/clipmonitor/Properties/Resources.Designer.cs
+++ apps/WPF/clipmonitor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace cmon.Properties
+namespace clipmonitor.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace cmon.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("cmon.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("clipmonitor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- apps/WPF/clipmonitor/Properties/Settings.Designer.cs
+++ apps/WPF/clipmonitor/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace cmon.Properties
+namespace clipmonitor.Properties
 {
 
 
