b47bc2898cd93cd83a3b886ffd8caa192f6d9abc
Горин Александр <pu1s@outlook.com>
2018-09-23 12:28:00 +0400

    +

---
+++ libs/ClipboardMonitorEngine/clipboard.monitor.engine/ClipboardMonitorWindow.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "ClipboardMonitorWindow.h"
+
+
+ClipboardMonitorWindow::ClipboardMonitorWindow()
+{
+}
---
+++ libs/ClipboardMonitorEngine/clipboard.monitor.engine/ClipboardMonitorWindow.h
@@ -0,0 +1,17 @@
+#pragma once
+#using <System.Windows.Forms.dll>
+#include "ClipboardMonitorWindowState.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+
+public ref class ClipboardMonitorWindow	: public System::Windows::Forms::Form
+{
+private:
+
+public:
+	ClipboardMonitorWindow();
+};
+
