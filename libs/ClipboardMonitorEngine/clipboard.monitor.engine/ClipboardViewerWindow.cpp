1979a554bcfc72649378b8fcc404b553352e8308
Горин Александр <pu1s@outlook.com>
2018-10-27 10:15:54 +0400

    works

--- libs/ClipboardMonitorEngine/clipboard.monitor.engine/ClipboardViewerWindow.cpp
+++ libs/ClipboardMonitorEngine/clipboard.monitor.engine/ClipboardViewerWindow.cpp
@@ -5,6 +5,9 @@ inline void pu1ssoft::ClipboardViewerWindow::WndProc(Message % msg)
 {
 	switch (msg.Msg)
 	{
+	case WM_CREATE:
+		System::Windows::Forms::MessageBox::Show("WM_CREATE");
+		break;
 	default:
 		DefWndProc(msg);
 		break;
