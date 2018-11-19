e080f96fcb2ff818f54ce5cb5b3abec078dc1626
pu1s <pu1s@outlook.com>
2018-11-19 15:36:57 +0400

    /*******/

--- ConsoleAppW32/ClipboardViewerEx.cpp
+++ ConsoleAppW32/ClipboardViewerEx.cpp
@@ -53,47 +53,7 @@ stdx::ClipboardViewerEx::ClipboardViewerEx(HINSTANCE hInstance, LPCWSTR window_n
 	
 }
 
-void stdx::ClipboardViewerEx::CreateWindowViewer(HINSTANCE hInstance, LPCWSTR window_name, LPCWSTR window_title, WNDPROC wndproc, HWND & handle) noexcept
-{
-	LPCWSTR szWinName = (LPCWSTR)window_name;				// Произвольное имя класса главного окна
-	LPCWSTR szTitle = (LPCWSTR)window_title;				// Произвольный заголовок окна
-																// Структура msg типа MSG для получения сообщений Windows
-																// Структура wc типа WNDCLASS для задания характеристик окна
 
-	/*Заполнение структуры wc типа WNDCLASS для описания класса главного окна*/
-	ZeroMemory(&_wc, sizeof(_wc));								// Обнуление всех членов структуры wc
-	_wc.hInstance = hInstance;                                   // Дескриптор приложения
-	_wc.lpszClassName = (LPCWSTR)szWinName;                      // Имя класса окна
-	_wc.lpfnWndProc = wndproc;								// Определение оконной функции 
-	_wc.style = 0;                                               // Стиль по умолчанию
-	_wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);					// Стандартная пиктограмма
-	_wc.hCursor = LoadCursor(NULL, IDC_ARROW);					// Стандартный курсор мыши
-	_wc.hbrBackground = GetStockBrush(BLACK_BRUSH);				// Черный фон окна
-	_wc.lpszMenuName = NULL;										// Без меню
-	_wc.cbClsExtra = 0;                                          // Без дополнительной информации
-	_wc.cbWndExtra = 0;                                          // Без дополнительной информации
-
-	/*Регистрация класса главного окна*/
-	if (!RegisterClass(&_wc))                                    // Если класс окна не регистрируется
-	{															// выводим сообщение и заканчиваем выполнение программы
-		MessageBox(NULL, L"Окно нерегестрируется", L"Ошибка", MB_OK);
-		return;
-	}                                                           // возвращаем код ошибки
-	/*Создание главного окна и отображение его на мониторе*/
-	_hWnd = CreateWindow(									// Вызов функции API для создания ок-на
-		(LPCWSTR)szWinName,                                     // имя класса главного окна
-		(LPCWSTR)szTitle,                                       // заголовок окна
-		WS_OVERLAPPEDWINDOW,									// Стиль окна 
-		200,                                                    // x-координата левого угла окна
-		200,                                                    // y-координата левого угла окна
-		500,                                                    // Ширина окна
-		100,                                                    // Высота окна
-		HWND_DESKTOP,											// Без родительского окна
-		NULL,                                                   // Без меню
-		hInstance,                                              // Дескриптор приложения
-		NULL);
-	handle = _hWnd;
-}
 
 bool stdx::ClipboardViewerEx::DestroyWindowViewer(void) noexcept
 {
--- ConsoleAppW32/ClipboardViewerEx.h
+++ ConsoleAppW32/ClipboardViewerEx.h
@@ -32,13 +32,6 @@ namespace stdx
 			LPCWSTR window_name,
 			LPCWSTR window_title,
 			WNDPROC wndproc);
-		void CreateWindowViewer(
-			HINSTANCE hInstance,
-			LPCWSTR window_name,
-			LPCWSTR window_title,
-			WNDPROC wndproc,
-			HWND& handle
-		) noexcept;
 
 		bool DestroyWindowViewer(void) noexcept;
 
